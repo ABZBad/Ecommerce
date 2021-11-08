ac1ae67203386df00d96e8ca11d937ff70b52fca
jorge468 <81258540+jorge468@users.noreply.github.com>
2021-11-08 17:56:58 -0600

    Delete Unit0-main directory

--- Unit0-main/src/main/java/com/marcoAMR/repository/CategoriasRepository.java
+++
@@ -1,9 +0,0 @@
-package com.marcoAMR.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.marcoAMR.model.Categoria;
-
-public interface CategoriasRepository extends JpaRepository<Categoria, Integer> {
-
-}
--- Unit0-main/src/main/java/com/marcoAMR/repository/PerfilesRepository.java
+++
@@ -1,9 +0,0 @@
-package com.marcoAMR.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.marcoAMR.model.Perfil;
-
-public interface PerfilesRepository extends JpaRepository<Perfil, Integer> {
-
-}
